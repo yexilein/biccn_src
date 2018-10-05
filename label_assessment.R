2498c66d11c6bcdd47b9944ffc7bb0c168a43e21
yexilein <stephischer@gmail.com>
2018-10-05 17:38:09 -0400

    Add local copy of Metaneighbor

--- label_assessment.R
+++ label_assessment.R
@@ -2,18 +2,56 @@
 source("utility.R", local = TRUE)
 
 assess_module_labels <- function(dataset, cell_types, modules, hvg) {
-  go_terms <- go_slim(rownames(dataset))
+  go_terms <- go_slim(rownames(dataset))[1:3]
   go_terms$hvg <- hvg
-  dataset <- dataset[unique(unlist(go_terms)),]
+  dataset <- dataset[rownames(dataset) %in% unique(unlist(go_terms)),]
+  module_labels <- create_module_labels(dataset$study_id, cell_types, modules)
+  call_my_metaneighbor(dataset, go_terms, module_labels)
+}
+
+call_metaneighbor <- function(dataset, go_terms, module_labels) {
   aurocs <- MetaNeighbor::MetaNeighbor(
     dat = dataset,
     experiment_labels = as.numeric(factor(dataset$study_id)),
-    celltype_labels = create_module_labels(dataset$study_id, cell_types, modules),
+    celltype_labels = module_labels,
     genesets = go_terms,
     bplot = FALSE,
     fast_version = TRUE
   )
   return(aurocs)
+}
+
+call_my_metaneighbor <- function(dataset, go_terms, module_labels) {
+  dat <- assay(dataset)
+  result <- list()
+  for(l in seq_along(go_terms)){
+    print(names(go_terms)[l])
+    subdat <- dat[rownames(dat) %in% go_terms[[l]],]
+    result[[l]] <- average_auroc(subdat, dataset$study_id, module_labels)
+  }
+  result <- do.call(rbind, result)
+  dimnames(result) <- list(names(go_terms), colnames(module_labels))
+  return(result)
+}
+
+average_auroc <- function(dat_sub, study_id, celltype_labels) {
+  dat_sub <- as.matrix(dat_sub)
+  nonzero_cells <- colSums(dat_sub) > 0
+  dat_sub <- dat_sub[, nonzero_cells]
+  study_id <- study_id[nonzero_cells]
+  celltype_labels <- celltype_labels[nonzero_cells,, drop = FALSE]
+  normalized_data <- normalize_cols(dat_sub)
+  aurocs <- c()
+  for (study in unique(study_id)) {
+    is_study <- study_id == study
+    votes <- crossprod(
+      normalized_data[, is_study],
+      normalized_data[, !is_study] %*% celltype_labels[!is_study,]
+    )
+    all_aurocs <- compute_aurocs(votes, celltype_labels[is_study,])
+    aurocs <- cbind(aurocs, diag(all_aurocs))
+  }
+  return(round(rowMeans(aurocs, na.rm = TRUE),3))
 }
 
 go_slim <- function(known_genes, min = 10, max = 1000) {
